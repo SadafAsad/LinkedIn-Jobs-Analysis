e97b295987230ab5fb8cd1ca14c32316452be72e
sdf <34832185+SadafAsad@users.noreply.github.com>
2024-03-26 09:34:51 -0400

    clean

--- jobscraper/jobscraper/spiders/jobs.py
+++ jobscraper/jobscraper/spiders/jobs.py
@@ -11,18 +11,24 @@ class JobsSpider(scrapy.Spider):
 
 
     def parse_job(self, response):
-        job_item = {}
+        # job_item = {}
         jobs = response.css('li')
 
         for job in jobs:
+
+            yield {
+                'job_title' : job.css("h3::text").get(default='not-found').strip(),
+                'job_url': job.css(".base-card__full-link::attr(href)").get(default='not-found').strip(),
+                'job_location': job.css('.job-search-card__location::text').get(default='not-found').strip()
+            }
             
-            job_item['job_title'] = job.css("h3::text").get(default='not-found').strip()
-            job_item['job_detail_url'] = job.css(".base-card__full-link::attr(href)").get(default='not-found').strip()
-            # job_item['job_listed'] = job.css('time::text').get(default='not-found').strip()   
-            # job_item['company_name'] = job.css('h4 a::text').get(default='not-found').strip()
-            # job_item['company_link'] = job.css('h4 a::attr(href)').get(default='not-found')
-            job_item['company_location'] = job.css('.job-search-card__location::text').get(default='not-found').strip()
-            yield job_item
+            # job_item['job_title'] = job.css("h3::text").get(default='not-found').strip()
+            # job_item['job_detail_url'] = job.css(".base-card__full-link::attr(href)").get(default='not-found').strip()
+            # # job_item['job_listed'] = job.css('time::text').get(default='not-found').strip()   
+            # # job_item['company_name'] = job.css('h4 a::text').get(default='not-found').strip()
+            # # job_item['company_link'] = job.css('h4 a::attr(href)').get(default='not-found')
+            # job_item['company_location'] = job.css('.job-search-card__location::text').get(default='not-found').strip()
+            # yield job_item
         
         first_job_on_page = response.meta['first_job_on_page']
 
