6a1f291e7f1df01d365567e749c427d6ac757d72
sdf <34832185+SadafAsad@users.noreply.github.com>
2024-03-26 10:03:02 -0400

    clean

--- jobscraper/jobscraper/spiders/jobs.py
+++ jobscraper/jobscraper/spiders/jobs.py
@@ -11,33 +11,18 @@ class JobsSpider(scrapy.Spider):
 
 
     def parse_job(self, response):
-        # job_item = {}
         jobs = response.css('li')
 
         for job in jobs:
             job_url = job.css(".base-card__full-link::attr(href)").get(default='not-found').strip()
             yield scrapy.Request(url=job_url, callback=self.parse_job_page)
 
-            # yield {
-            #     'job_title' : job.css("h3::text").get(default='not-found').strip(),
-            #     'job_url': job.css(".base-card__full-link::attr(href)").get(default='not-found').strip(),
-            #     'job_location': job.css('.job-search-card__location::text').get(default='not-found').strip()
-            # }
-            
-            # job_item['job_title'] = job.css("h3::text").get(default='not-found').strip()
-            # job_item['job_detail_url'] = job.css(".base-card__full-link::attr(href)").get(default='not-found').strip()
-            # # job_item['job_listed'] = job.css('time::text').get(default='not-found').strip()   
-            # # job_item['company_name'] = job.css('h4 a::text').get(default='not-found').strip()
-            # # job_item['company_link'] = job.css('h4 a::attr(href)').get(default='not-found')
-            # job_item['company_location'] = job.css('.job-search-card__location::text').get(default='not-found').strip()
-            # yield job_item
-
-            first_job_on_page = response.meta['first_job_on_page']
-            num_jobs_returned = len(jobs)
-            if num_jobs_returned > 0:
-                first_job_on_page = int(first_job_on_page) + num_jobs_returned
-                next_url = self.api_url + str(first_job_on_page)
-                yield scrapy.Request(url=next_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
+        first_job_on_page = response.meta['first_job_on_page']
+        num_jobs_returned = len(jobs)
+        if num_jobs_returned > 0:
+            first_job_on_page = int(first_job_on_page) + num_jobs_returned
+            next_url = self.api_url + str(first_job_on_page)
+            yield scrapy.Request(url=next_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
 
     def parse_job_page(self, response):
         pass
